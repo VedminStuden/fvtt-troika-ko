490da8286f65b0d64e5a1ac5276bbedd0b17177c
VedminStuden <140993177+VedminStuden@users.noreply.github.com>
2024-04-16 08:39:08 +0900

    Update item.js

--- module/item/item.js
+++ module/item/item.js
@@ -18,12 +18,12 @@ export class TroikaItem extends Item {
 
             if(item.system.requiresTwoHands === true){
                 item.system.displayName += "*";
-                item.system.attackTooltip += "This item requires two hands to wield.\n"
+                item.system.attackTooltip += "이 아이템은 적어도 양손으로 잡아야 합니다.\n"
             }
 
             if(item.system.armourIgnored > 0){
                 item.system.displayName += "#";
-                item.system.attackTooltip += `This item bypasses ${item.system.armourIgnored} point(s) of armour.`
+                item.system.attackTooltip += `이 아이템은 ${item.system.armourIgnored} 만큼의 장갑을 관통합니다.`
             }
 
             //console.log(item);
